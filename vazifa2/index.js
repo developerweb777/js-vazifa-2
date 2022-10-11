fc3d952e07c0f23081689e10b1d9a0e1de4b8267
developerweb777 <nosirovkamolxon308@gmail.com>
2022-10-11 23:27:49 +0300

    js2vazifa

---
+++ vazifa2/index.js
@@ -0,0 +1,33 @@
+// 1--masala
+// let a=527;
+// console.log(a/100);
+// 2--masala
+// let m=1000;
+// let d=m/1000;
+// console.log(d);
+// 3--masala
+// let a=884;
+// let b=183;
+// console.log(a/b);
+// 4--masala
+// let a=28;
+// console.log(Math.floor(a/10));
+// console.log(a%10);
+// 5--masala
+//  let b=56;
+// let d=Math.floor(b/10);
+// let f=b%10;
+// console.log(d+f);
+// 6--masala
+//  let b=56;
+// let d=Math.floor(b/10);
+// let f=b%10;
+// console.log(d*f);
+// 7--masala
+// let a=2435;
+// console.log(Math.floor(a/100)%10+"yuzlik");
+// 8--masala
+
+// let a=7200;
+// console.log(a/3600+"soat");
+// console.log(a/60+"daqiqa");
